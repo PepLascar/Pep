fa4f5105cb70e36558e698a0b14364fdc4173b82
unknown <peplascar@gmail.com>
2020-06-14 22:55:19 -0400

    ejercicio 2

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,14 @@
-buena pep
+print("Bienvenido al contador de frases")
+cont = 0
+continuar = "S"
+listafrase = [] 
+while(continuar=="S"):
+    frase = input("Ingrese la frase para contar sus letras: ")
+    listafrase.append(frase) 
+    cantidad = len(frase)
+    print(f"El número de letras de la frase es: {  cantidad  }")
+    cont = cont + 1
+
+    continuar = input("Desea continuar S/N").upper()
+
+print(listafrase)
