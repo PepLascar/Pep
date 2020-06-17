4939273e05c5aa3736c020b02eebcdbe78b377b3
Guru <rodviviano@gmail.com>
2020-06-16 21:42:05 -0400

    funcion que se llama a si misma

--- palindroma.py
+++ palindroma.py
@@ -1,16 +1,21 @@
-print("Palindroma Validator")
-palindroma = "no"
-continuar = "S"
-while(continuar=="S"):
+def palindromaFunction():
+    palindroma  =   "no"
+    x           =   0
     frase = input("Ingrese una frase: ")
-
-    for x in range(0,len(frase)):
-        for y in range(len(frase),0,-1):
-            if(frase[x]==frase[y-1]):
+    for y in range(len(frase)-1,0,-1):
+        print(f"frase[X]={ frase[x] }, frase[y]={ frase[y] }")
+        if(frase[x]==frase[y]):
                 palindroma="si"
-            else:
-                palindroma="no"
-
+        else:
+            palindroma="no"
+            break
+        x=x+1
+        if(x==y):
+            break       
     print(f"La frase {  palindroma  } es palindroma.")
+    continuar = input("Desea continuar S/N: ").upper()
+    if(continuar=="S"):
+        palindromaFunction()
 
-    continuar = input("Desea continuar S/N").upper()
+print("Palindroma Validator")
+palindromaFunction()
