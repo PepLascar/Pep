0d63559130b84f91b7890c6db0b3706e022e68fa
Guru <rodviviano@gmail.com>
2020-06-17 23:54:18 -0400

    ejercicio 3

---
+++ alf_3_guru.py
@@ -0,0 +1,16 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista,
+# pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> 
+# has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas 
+# introducidas por el usuario.
+import numpy
+
+def alf3():
+    asignatura = ["Matematicas", "Lenguaje", "Fisica"]
+    nota = numpy.empty(len(asignatura), dtype='float')
+    for i in range(0,len(asignatura)):
+        nota[i] = float(input(f"Que nota te sacaste en {asignatura[i]}: "))
+    
+    for i in range(0,len(asignatura)):
+        print(f"En {asignatura[i]} has sacado {(nota[i])}")
+
+alf3()
