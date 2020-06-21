c0048dd199400b77c6adcb3e5113dd9a9e60a40c
Guru <rodviviano@gmail.com>
2020-06-20 21:07:37 -0400

    file base + base ej 6

---
+++ alf_6_guru.py
@@ -0,0 +1,8 @@
+def ejercicio6():
+    
+    #Condicion para volver a ejecutar la funcion
+    continuar = input("Si desea continuar escriba cualquier wea:")
+    if(continuar!=""):
+        ejercicio6()
+
+ejercicio6()
