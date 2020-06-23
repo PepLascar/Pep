917465d408315e86b61a7c1854a795ba45601d4d
unknown <peplascar@gmail.com>
2020-06-23 15:31:33 -0400

    ej_2

--- ej_actividad_2.py
+++ ej_actividad_2.py
@@ -1,33 +1,38 @@
-import numpy
-personas = numpy.empty(10, dtype='str')
-limite = 0
-indice = 0
+def reset():
+    import os
+    os.system ("cls")
+    import numpy
+    personas = numpy.empty(10, dtype='str')
+    limite = 0
+    indice = 0
+    continuar = "S"
 
-loop = "S"
-while( loop == "S"):
-    print("Menu:\n----------------------------------------")
-    opcion = input("1) Presione 1 para agregar persona.\n2) Presione 2 para ver persona.\n")
+    while(continuar == "S"):
+        print("\nMenu:\n------------------------------------")
+        opcion = input("1) Presione 1 para agregar persona.\n2) Presione 2 para ver persona.\n")
+        if(opcion=="1" and limite<10):
+            nombre = input(str("Ingrese nombre: "))
+            personas[limite] = nombre
+            limite = limite + 1
 
+        if(limite > 10 and opcion =="1"):
+            print("Alcanzó limite máximo de nombres.")
 
-    if(opcion=="1" and limite<10):
-        nombre = input(str("Ingrese nombre: "))
-        personas[limite] = nombre
-        limite = limite + 1
-        
+        try:
+            if(opcion=="2"):
+                indice = int(input("Ingrese índice que desea ver: "))
+                print(personas[indice])
 
-    if(limite > 10 and opcion =="1"):
-        print("Alcanzó limite máximo de nombres.")
+        except:
+            if(indice > 10):
+                print("Ingrese un índice menor a 10")
+                continuar = input("¿Desea continuar? S/N: ").upper()
+                if(continuar == "S"):
+                    reset()
+                else:
+                    print("Fin del programa")
 
-    try:
-        if(opcion=="2"):
-            indice = int(input("Ingrese índice que desea ver: "))
-            print(personas[indice])
-
-    except:
-        if(indice > 10):
-            print("Ingrese un índice menor a 10")
-            loop = input("¿Desea continuar? S/N").upper()
-            
+reset()    
 
         
             
