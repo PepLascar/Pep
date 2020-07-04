44b9246d6372835fbbc15b36119b27a8de7c2c1c
Guru <rodviviano@gmail.com>
2020-07-03 23:25:53 -0400

    3 opt

--- full.py
+++ full.py
@@ -7,38 +7,44 @@ empleados_asesorias = numpy.empty(5, dtype='object')
 
 departamentos = ["Gerencia","Marketing","Producción","RRHH","Finanzas"]
 empresas = ["Empresa Master Retail","Empresa Master Asesorías"]
+flag=1
+while(flag==1):
+    opcion = input("\nIngrese una opcion\n1) Registrar empleados.\n2) TOTAL Y PROMEDIO.\n3) LISTAR TODO.\n")
 
-for x in range(0,5):
-    empleados_retail[x]=x+1
-    empleados_asesorias[x]=x+2
+    if(opcion=="1"):
+        print(empresas[0])
+        for i in range(0,5):
+            empleados_retail[i] = int(input(f"Ingrese número empleados de {departamentos[i]}: "))
+        print(empresas[1])
+        for i in range(0,5):
+            empleados_asesorias[i] = int(input(f"Ingrese número empleados de {departamentos[i]}: "))
+        
+    if (opcion=="2"):
+        print("------------------------------------------------------------------------")
+        print(f"Total de empleados en el grupo Master IT: {sum(empleados_retail+empleados_asesorias)}")
+        for i in range(0,5):
+            print(f"Promedio de empleados de {departamentos[i]}: {(empleados_retail[i]+empleados_asesorias[i])/2}")
+    
 
-opcion=2
-if (opcion==2):
-    print("------------------------------------------------------------------------")
-    print(f"Total de empleados en el grupo Master IT: {sum(empleados_retail+empleados_asesorias)}")
-    for i in range(0,5):
-        print(f"Promedio de empleados de {departamentos[i]}: {(empleados_retail[i]+empleados_asesorias[i])/2}")
-   
-opcion = 3
-if (opcion==3):
-    print("------------------------------------------------------------------------")
-    print(empresas[0])
-    for j in range(0,5):
-        print(f"{departamentos[j]}", end="\t")
-    print()
-    for j in range(0,5):
-        if (j<3):  
-            print(f"{empleados_retail[j]}", end="\t\t")
-        else:
-            print(f"{empleados_retail[j]}", end="\t")
+    if (opcion=="3"):
+        print("------------------------------------------------------------------------")
+        print(empresas[0])
+        for j in range(0,5):
+            print(f"{departamentos[j]}", end="\t")
+        print()
+        for j in range(0,5):
+            if (j<3):  
+                print(f"{empleados_retail[j]}", end="\t\t")
+            else:
+                print(f"{empleados_retail[j]}", end="\t")
 
-    print("\n------------------------------------------------------------------------")
-    print(empresas[1])
-    for j in range(0,5):
-        print(f"{departamentos[j]}", end="\t")
-    print()
-    for n in range(0,5):
-        if (n<3):  
-            print(f"{empleados_asesorias[n]}", end="\t\t")
-        else:
-            print(f"{empleados_asesorias[n]}", end="\t")
+        print("\n------------------------------------------------------------------------")
+        print(empresas[1])
+        for j in range(0,5):
+            print(f"{departamentos[j]}", end="\t")
+        print()
+        for n in range(0,5):
+            if (n<3):  
+                print(f"{empleados_asesorias[n]}", end="\t\t")
+            else:
+                print(f"{empleados_asesorias[n]}", end="\t")
