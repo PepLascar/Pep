b479c23b1f6b5e36b9c8b373d0a86cf6010dabd1
unknown <peplascar@gmail.com>
2020-07-03 21:54:52 -0400

    act

--- ejercicio_1_prueba.py
+++ ejercicio_1_prueba.py
@@ -6,15 +6,28 @@
 #todo el grupo empresarial, (ejemplo: promedio de empleados de gerencia, promedio de empleados de marketing y otros) .
 
 #Recuerde validar la información para el ingreso de la cantidad de empleados (mayor a cero) y evitar ingresar texto en los diferentes datos de entrada del sistema.
+import os
+os.system("cls")
+
+import numpy
+empleados_retail = numpy.empty(5, dtype='object')
+empleados_asesorias = numpy.empty(5, dtype='object')
+
+departamentos =  ["Gerencia", "Marketing","Producción", "RRHH", "Finanzas"]
+empresas = ["Empresa Master Retail", "Empresa Master Asesorías"]
+
+
+print("------------------------------------")
+for i in range(0,2):
+    print(empresas[i])
+    for j in range(0,5):
+        print(f"{departamentos[j]}", end="\t")
+    print("\n------------------------------------")
 
 
 
-import numpy as np  
-empresas = np.empty([2, 5], dtype='int')
 
-for i in range(2):  #empresas
-    for j in range(5):  #departamentos
-        empleados = input(f"Ingrese nuevo colaborador: {i} {j}")
-        empresas [i, j] = empleados
+
+
 
 #registrar cantidad empleados
