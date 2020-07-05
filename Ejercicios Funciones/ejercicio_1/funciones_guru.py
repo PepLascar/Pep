7404bac78894185826f626366c2ad450f4e26595
Guru <rodviviano@gmail.com>
2020-07-05 19:05:39 -0400

    ejercicio completado

--- Ejercicios Funciones/ejercicio_1/funciones_guru.py
+++ Ejercicios Funciones/ejercicio_1/funciones_guru.py
@@ -1,9 +1,24 @@
 
-def calcularIva():
-    print("esta es la funcion calcularIva")
+def calcularIva(iva):
+    iva=iva*0.19
+    return int(iva)
 
-def descuento():
-    print("esta es la funcion descuento")
+def descuento(precio,descuento):
+    precio=precio - int(precio*(descuento/100))
+    return precio
 
-def calcularImc():
-    print("esta es la funcion calcularImc")
+def calcularImc(altura,peso):
+    imc=peso/(altura*altura)
+    if(imc < 18.5):
+        estado="Bajo peso"
+    if(imc >= 18.5 and imc < 25.0):
+        estado="Adecuado"
+    if(imc >= 25.0 and imc < 30.0):
+        estado="Sobrepeso"
+    if(imc >= 30.0 and imc < 35.0):
+        estado="Obesidad grado 1"
+    if(imc >= 35.0 and imc < 40.0):
+        estado="Obesidad grado 2"
+    if(imc >= 40.0):
+        estado="Obesidad grado 3"
+    return estado
--- Ejercicios Funciones/ejercicio_1/main_guru.py
+++ Ejercicios Funciones/ejercicio_1/main_guru.py
@@ -5,13 +5,19 @@ while(flag==1):
     opcion = input("\nIngrese una opcion\n1) Calcular Iva.\n2) Descuento.\n3) Calcular Imc.\n4) Salir.\n")
 
     if(opcion=="1"):
-        funciones.calcularIva()
+        iva = int(input("Ingrese el valor del producto para calcular el IVA: "))
+        print(f"El IVA es: {funciones_guru.calcularIva(iva)}")
         
     if (opcion=="2"):
-        print("ejecuto funcion 2")
-
+        precio = int(input("Ingrese el valor del producto: "))
+        descuento = int(input("Ingrese el descuento que se le aplicara al producto: "))
+        print(f"El precio con el descuento aplicado es: {funciones_guru.descuento(precio,descuento)}")
+    
     if (opcion=="3"):
-        print("ejecuto funcion 3")
+        altura = float(input("Ingrese su altura en metros: "))
+        peso = float(input("Ingrese su peso en kilos: "))
+        print(f"De acuerdo el IMC su estado es: {funciones_guru.calcularImc(altura,peso)}")
+
 
     if (opcion=="4"):
         flag=0
