2ebef522dc97fb372054883cb7caf56b95f3c20a
Guru <rodviviano@gmail.com>
2020-07-05 18:01:39 -0400

    guru's part

---
+++ Ejercicios Funciones/ejercicio_1/funciones_guru.py
@@ -0,0 +1,9 @@
+
+def calcularIva():
+    print("esta es la funcion calcularIva")
+
+def descuento():
+    print("esta es la funcion descuento")
+
+def calcularImc():
+    print("esta es la funcion calcularImc")
